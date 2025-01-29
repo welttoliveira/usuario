b5e06c4d9e6fd395255ad287771a34015fe5bd23
welttoliveira <welttoliveira@gmail.com>
2025-01-29 11:48:18 -0300

    adicionando metodos get, delete e login

---
+++ src/main/java/com/javanauta/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.javanauta.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String mensagem){
+        super(mensagem);
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
